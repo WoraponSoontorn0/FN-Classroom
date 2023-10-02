2a9b6e42a247dc11e0fad72de259b04623c58e94
WoraponSoontorn0 <worapon.so.63@ubu.ac.th>
2023-10-03 00:44:05 +0700

    correct

--- classroom/urls.py
+++ classroom/urls.py
@@ -3,7 +3,4 @@ from classroom import views
 
 urlpatterns = [
      path('', views.home, name="home"),
-     path('page_one/', views.page_one, name='page_one'),
-     path('page_two/', views.page_two, name='page_two'),
-     path('send_kafka_message/', views.send_kafka_message, name='send_kafka_message'),
 ]
--- classroom/views.py
+++ classroom/views.py
@@ -1,29 +1,41 @@
+import time
+import json
 from django.shortcuts import render, redirect
+from kafka import KafkaConsumer, KafkaProducer
 from classroom.models import *
-from django.shortcuts import render
-from django.http import HttpResponse
-from confluent_kafka import Producer
 
-kafka_producer = Producer({
-    'bootstrap.servers': 'localhost:9092',  
-})
-
-def send_kafka_message(req):
-    if req.method == 'POST':
+def get_message():
+    consumer = KafkaConsumer('homework',
+                             bootstrap_servers='localhost:9092',
+                             consumer_timeout_ms=10000,
+                             group_id='_id.hw')
+    
+    messages = []
+    for msg in consumer:
         try:
-            kafka_producer.produce('MEY', key='your_key', value='your_message')
-            kafka_producer.flush()
-            print("เสร็จสิ้น")
-            return render(req, 'classroom/one.html')
-        except Exception as e:
-            return HttpResponse(f"Error sending Kafka message: {str(e)}")
-    return HttpResponse("Invalid request method.")
+            data = json.loads(msg.value)
+            if 'message' in data and data['message'] != ':':
+                messages.append(data['message'])
+        except json.JSONDecodeError:
+            pass
 
-def page_one(req):
-    return render(req, 'classroom/one.html')
+    consumer.close()
+    return messages
 
-def home(req):
-    return render(req, 'classroom/home.html')
+def send_message(message):
+    producer = KafkaProducer(
+        bootstrap_servers='localhost:9092',
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    )
+    time.sleep(1)
+    producer.send('homework', {'message': message})
+    producer.flush()
+    producer.close()
 
-def page_two(req):
-    return render(req, 'classroom/two.html')
+def home(request):
+    if request.method == 'POST':
+        my_homework = request.POST.get('hw')
+        send_message(my_homework)
+        
+    my_homework = get_message()
+    return render(request, 'classroom/home.html', {'my_homework': my_homework})
